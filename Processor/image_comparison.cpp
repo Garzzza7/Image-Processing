84f452098ea287b5ffa145f3c2f7e308d2bff6a2
Garzzza7 <piotr.czapela@gmail.com>
2022-10-18 19:06:56 +0200

    dsa

--- Processor/image_comparison.cpp
+++ Processor/image_comparison.cpp
@@ -11,7 +11,7 @@ float maximum_difference();
 */
 float mean_square_error(){
     CImg<unsigned char> image1("..\\..\\images\\lenac.bmp");
-    CImg<unsigned char> image2("..\\..\\images\\lenac.bmp");
+    CImg<unsigned char> image2("..\\..\\images\\lenac_blur.bmp");
     float result=0;
     float dimensions = image1.width()*image1.height();
     for (int x = 0; x < image1.width(); x++) {
@@ -23,10 +23,9 @@ float mean_square_error(){
 
 }
 float peak_mean_square_error(){
-    CImg<unsigned char> image1("..\\..\\images\\lenac.bmp");
-    CImg<unsigned char> image2("..\\..\\images\\lenac.bmp");
+    CImg<unsigned char> image1("..\\..\\images\\lena1.bmp");
+    CImg<unsigned char> image2("..\\..\\images\\lena2.bmp");
     float max;
-    float max2= max*max;
     float result=0;
     float dimensions = image1.width()*image1.height();
     for (int x = 0; x < image1.width(); x++) {
@@ -34,53 +33,59 @@ float peak_mean_square_error(){
             if (image1(x,y)>max){
                 max=image1(x,y);
             }
-            result = result +(image1(x,y)-image2(x,y))*(image1(x,y)-image2(x,y));
+            result +=(image1(x,y)-image2(x,y));
         }
     }
-    return result/(dimensions * max2);
+    return result*result/(dimensions * max*max);
 }
 float signal_to_noise_ratio(){
-    CImg<unsigned char> image1("..\\..\\images\\lenac.bmp");
-    CImg<unsigned char> image2("..\\..\\images\\lenac.bmp");
+    CImg<unsigned char> image1("..\\..\\images\\lena1.bmp");
+    CImg<unsigned char> image2("..\\..\\images\\lena2.bmp");
     float result1=0;
     float result2=0;
     for (int x = 0; x < image1.width(); x++) {
         for (int y = 0; y < image1.height(); y++) {
-            result1=result1+(image1(x,y)*image1(x,y)); // yep i dont trust
+            result1+=image1(x,y); // yep i dont trust
         }
     }
     for (int x = 0; x < image1.width(); x++) {
         for (int y = 0; y < image1.height(); y++) {
-            result1=result1+((image1(x,y)+image1(x,y))-(image2(x,y)+image2(x,y))*(image1(x,y)+image1(x,y))-(image2(x,y)+image2(x,y))); // yep i dont trust
+            result2+=(image1(x,y)-image2(x,y)); // yep i dont trust
         }
     }
-    return 10*log10(result1/result2);
+    return 10*log10((result1*result1)/(result2*result2));
+    //return 10*log10((result1*result1)/(result2*result2));
 }
 float peak_signal_to_noise_ratio(){
-    CImg<unsigned char> image1("..\\..\\images\\lenac.bmp");
-    CImg<unsigned char> image2("..\\..\\images\\lenac.bmp");
+    CImg<unsigned char> image1("..\\..\\images\\lena1.bmp");
+    CImg<unsigned char> image2("..\\..\\images\\lena2.bmp");
     float max;
-    float max2=max*max;
     float result1=0;
+    float result2=0;
     for (int x = 0; x < image1.width(); x++) {
         for (int y = 0; y < image1.height(); y++) {
             if (image1(x,y)>max){
                 max=image1(x,y);
             }
+            result2+=max;
 
         }
     }
-    for (int x = 0; x < image1.width(); x++) {
-        for (int y = 0; y < image1.height(); y++) {
-            result1=result1+((image1(x,y)+image1(x,y))-(image2(x,y)+image2(x,y))*(image1(x,y)+image1(x,y))-(image2(x,y)+image2(x,y))); // yep i dont trust
+    for (int x = 0; x < image2.width(); x++) {
+        for (int y = 0; y < image2.height(); y++) {
+            result1+=(image1(x,y)-image2(x,y)); // yep i dont trust
         }
     }
+    float max2=max*max;
+    //return 10*log10(result2*result2/result1);
+    //return 10*log10((result2*result2)/(result1*result1));
+    return 10*log10(()/(result1*result1);
 
-    return 10*log10(max2/result1);
+    //  return result2;
 }
 float maximum_difference(){
     CImg<unsigned char> image1("..\\..\\images\\lenac.bmp");
-    CImg<unsigned char> image2("..\\..\\images\\lenac.bmp");
+    CImg<unsigned char> image2("..\\..\\images\\lenac_blur.bmp");
     float result=0;
     for (int x = 0; x < image1.width(); x++) {
         for (int y = 0; y < image1.height(); y++) {
