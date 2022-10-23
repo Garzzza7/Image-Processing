60b1f7768bdc838e87b67ea60e01dd47da88dc9d
Garzzza7 <piotr.czapela@gmail.com>
2022-10-23 15:22:10 +0200

    PIWO ALKOHOL AAAAAAAAAAAAAAAAA

--- Processor/main.cpp
+++ Processor/main.cpp
@@ -8,7 +8,11 @@
 #include "image_comparison.cpp"
  */
 #include "Task1.h"
+#include "popl.hpp"
 using namespace cimg_library;
+using namespace popl;
+using namespace std;
+
 /*
 void horizontal_flip();
 void vertical_flip();
@@ -47,7 +51,7 @@ int main() {
     return 0;
 }
 */
-int main() {
+int main(int argc, char **argv) {
     //negative();
     /*
     brightness_modification(100);
@@ -67,9 +71,66 @@ int main() {
     std::cout<<peak_signal_to_noise_ratio()<<std::endl;
     std::cout<<maximum_difference()<<std::endl;
      */
+   // adaptive_median_filter();
 
 
-    adaptive_median_filter();
+
+
+    //https://github.com/badaix/popl
+    float f;
+    int m, i;
+    bool v;
+
+    OptionParser op("Allowed options");
+    auto help_option     = op.add<Switch>("h", "help", "produce help message");
+    auto verbose_option  = op.add<Switch>("v", "verbose", "be verbose", &v);
+    auto hidden_option   = op.add<Switch, Attribute::hidden>("x", "", "hidden option");
+    auto double_option   = op.add<Value<double>>("d", "double", "test for double values", 3.14159265359);
+    auto float_option    = op.add<Value<float>>("f", "float", "test for float values", 2.71828182845f, &f);
+    op.add<Value<int>>("i", "int", "test for int value w/o option", 23, &i);
+    auto string_option   = op.add<Value<string>>("s", "string", "test for string values");
+    auto implicit_int_option = op.add<Implicit<int>>("m", "implicit", "implicit test", 42);
+    auto advanced_option = op.add<Switch, Attribute::advanced>("", "advanced", "advanced option");
+    auto expert_option   = op.add<Switch, Attribute::expert>("", "expert", "expert option");
+    auto inactive_option = op.add<Switch>("", "inactive", "inactive option");
+    inactive_option->set_attribute(Attribute::inactive);
+    implicit_int_option->assign_to(&m);
+
+    op.parse(argc, argv);
+
+    // print auto-generated help message
+    if (help_option->count() == 1)
+        cout << op << "\n";
+    else if (help_option->count() == 2)
+        cout << op.help(Attribute::advanced) << "\n";
+    else if (help_option->count() > 2)
+        cout << op.help(Attribute::expert) << "\n";
+
+    // show all non option arguments (those without "-o" or "--option")
+    for (const auto& non_option_arg: op.non_option_args())
+        cout << "non_option_args: " << non_option_arg << "\n";
+
+    // show unknown options (undefined ones, like "-u" or "--undefined")
+    for (const auto& unknown_option: op.unknown_options())
+        cout << "unknown_options: " << unknown_option << "\n";
+
+    // print all the configured values
+    cout << "verbose_option  - is_set: " << verbose_option->is_set() << ", count: " << verbose_option->count() << ", reference: " << v << "\n";
+    cout << "hidden_option   - is_set: " << hidden_option->is_set() << ", count: " << hidden_option->count() << "\n";
+    cout << "double_option   - is_set: " << double_option->is_set() << ", count: " << double_option->count() << ", value: " << double_option->value() << "\n";
+    cout << "string_option   - is_set: " << string_option->is_set() << ", count: " << string_option->count() << "\n";
+    if (string_option->is_set())
+    {
+        for (size_t n=0; n<string_option->count(); ++n)
+            cout << "string_option #" << n << " - value: " << string_option->value(n) << "\n";
+    }
+    cout << "float_option    - is_set: " << float_option->is_set() << ", value: " << float_option->value() << ", reference: " << f << "\n";
+    cout << "int w/o option  - reference: " << i << "\n";
+    auto int_option = op.get_option<Value<int>>('i');
+    cout << "int_option      - is_set: " << int_option->is_set() << ", value: " << int_option->value() << ", reference: " << i << "\n";
+    cout << "imp_int_option  - is_set: " << implicit_int_option->is_set() << ", value: " << implicit_int_option->value() << ", reference: " << m << "\n";
+    cout << "advanced_option - is_set: " << advanced_option->is_set() << ", count: " << advanced_option->count() << "\n";
+    cout << "expert_option   - is_set: " << expert_option->is_set() << ", count: " << expert_option->count() << "\n";
 
 
 
