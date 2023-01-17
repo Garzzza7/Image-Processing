82dc68de3137e8f797cc5bdca4ef664455bc5755
olabanasiak <234750@edu.p.lodz.pl>
2023-01-17 17:30:30 +0100

    FFT and IFFT done

--- Processor/task4.cpp
+++ Processor/task4.cpp
@@ -8,6 +8,7 @@
 #include "CImg.h"
 #include "Task1.h"
 #include <math.h>
+
 using namespace std;
 
 void fast_FT(CImg<unsigned char> &image){
@@ -72,66 +73,177 @@ void slow_dicrete_IDFT(CImg<unsigned char> &image){
     buffer.save_bmp("..\\..\\images\\slow_dicrete_IDFT.bmp");
 }
 
+void fft(std::vector<std::complex<double>> &arr, int n) {
 
-
-
-// Perform in-place FFT on the given complex array using the Cooley-Tukey algorithm
-void FFT(complex<double> *a, int n) {
-    // Base case: if n == 1, return
     if (n == 1) return;
 
-    // Split the array into even and odd halves
-    complex<double> *a_even = new complex<double>[n/2];
-    complex<double> *a_odd = new complex<double>[n/2];
+    std::vector<std::complex<double>> even(n/2);
+    std::vector<std::complex<double>> odd(n/2);
     for (int i = 0; i < n/2; i++) {
-        a_even[i] = a[2*i];
-        a_odd[i] = a[2*i + 1];
+        even[i] = arr[2*i];
+        odd[i] = arr[2*i + 1];
     }
+    fft(even, n/2);
+    fft(odd, n/2);
 
-    // Recursively compute the FFT of the even and odd halves
-    FFT(a_even, n/2);
-    FFT(a_odd, n/2);
-
-    // Combine the results using the Cooley-Tukey algorithm
+    double theta = -2.0 * M_PI /(double)n;
+    std::complex<double> v(1);
+    std::complex<double> wn(cos(theta), sin(theta));
     for (int k = 0; k < n/2; k++) {
-        complex<double> t = polar(1.0, -2 * M_PI * k / n) * a_odd[k];
-        a[k] = a_even[k] + t;
-        a[k + n/2] = a_even[k] - t;
+        arr[k] = even[k] + v * odd[k];
+        arr[k + n/2] = even[k] - v * odd[k];
+        v *= wn;
+    }
+}
+
+std::vector<std::vector<std::complex<double>>> FFT(CImg<unsigned char> &image) {
+
+    CImg<unsigned char> buffer=image;
+    const int ROWS = (int)image.width();
+    std::vector<std::vector<std::complex<double>>> img;
+    img.resize(image.width());
+    for(int i = 0; i < image.width(); i++){
+        img[i].resize(image.height());
     }
 
-    // Clean up
-    delete[] a_even;
-    delete[] a_odd;
+    cimg_library::CImg<unsigned char>::iterator it;
+    for(int i = 0; i < image.width(); i++){
+        it = image.get_shared_channel(i).begin();
+        for(int j = 0; j < image.height(); j++, ++it) {
+            img[i][j] = std::complex<double>(*it,0);
+        }
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        fft(img[i], ROWS);
+    }
+
+    std::vector<std::complex<double>> temp(ROWS);
+    for (int j = 0; j < ROWS; j++) {
+        for (int i = 0; i < ROWS; i++) {
+            temp[i] = img[i][j];
+        }
+        fft(temp, ROWS);
+        for (int i = 0; i < ROWS; i++) {
+            img[i][j] = temp[i];
+        }
+    }
+    return img;
 }
+
+void ifft(std::vector<std::complex<double>> &arr, int n) {
+    if (n == 1) return;
+
+    std::vector<std::complex<double>> even(n/2);
+    std::vector<std::complex<double>> odd(n/2);
+    for (int i = 0; i < n/2; i++) {
+        even[i] = arr[2*i];
+        odd[i] = arr[2*i + 1];
+    }
+    fft(even, n/2);
+    fft(odd, n/2);
+
+    double theta = 2.0 * M_PI /(double)n;
+    std::complex<double> v(1);
+    std::complex<double> wn(cos(theta), -sin(theta));
+    for (int k = 0; k < n/2; k++) {
+        arr[k] = even[k] + v * odd[k];
+        arr[k + n/2] = even[k] - v * odd[k];
+        v *= wn;
+    }
+}
+
+std::vector<std::vector<std::complex<double>>> IFFT(CImg<unsigned char> &image) {
+
+    const int ROWS = (int)image.width();
+    std::vector<std::vector<std::complex<double>>> img;
+    img.resize(image.width());
+    for(int i = 0; i < image.width(); i++){
+        img[i].resize(image.height());
+    }
+
+    cimg_library::CImg<unsigned char>::iterator it;
+    for(int i = 0; i < image.width(); i++){
+        it = image.get_shared_channel(i).begin();
+        for(int j = 0; j < image.height(); j++, ++it) {
+            img[i][j] = std::complex<double>(*it,0);
+        }
+    }
+    for (int i = 0; i < ROWS; i++) {
+        ifft(img[i], ROWS);
+    }
+
+    std::vector<std::complex<double>> temp(ROWS);
+    for (int j = 0; j < ROWS; j++) {
+        for (int i = 0; i < ROWS; i++) {
+            temp[i] = img[i][j];
+        }
+        ifft(temp, ROWS);
+        for (int i = 0; i < ROWS; i++) {
+            img[i][j] = temp[i];
+        }
+    }
+
+    return img;
+}
+
+//// Perform in-place FFT on the given complex array using the Cooley-Tukey algorithm
+//void FFT(complex<double> *a, int n) {
+//    // Base case: if n == 1, return
+//    if (n == 1) return;
+//
+//    // Split the array into even and odd halves
+//    complex<double> *a_even = new complex<double>[n/2];
+//    complex<double> *a_odd = new complex<double>[n/2];
+//    for (int i = 0; i < n/2; i++) {
+//        a_even[i] = a[2*i];
+//        a_odd[i] = a[2*i + 1];
+//    }
+//
+//    // Recursively compute the FFT of the even and odd halves
+//    FFT(a_even, n/2);
+//    FFT(a_odd, n/2);
+//
+//    // Combine the results using the Cooley-Tukey algorithm
+//    for (int k = 0; k < n/2; k++) {
+//        complex<double> t = polar(1.0, -2 * M_PI * k / n) * a_odd[k];
+//        a[k] = a_even[k] + t;
+//        a[k + n/2] = a_even[k] - t;
+//    }
+//
+//    // Clean up
+//    delete[] a_even;
+//    delete[] a_odd;
+//}
 
 // Perform in-place IFFT on the given complex array using the Cooley-Tukey algorithm
-void IFFT(complex<double> *a, int n) {
-    // Base case: if n == 1, return
-    if (n == 1) return;
-
-    // Split the array into even and odd halves
-    complex<double> *a_even = new complex<double>[n/2];
-    complex<double> *a_odd = new complex<double>[n/2];
-    for (int i = 0; i < n/2; i++) {
-        a_even[i] = a[2*i];
-        a_odd[i] = a[2*i + 1];
-    }
-
-    // Recursively compute the IFFT of the even and odd halves
-    IFFT(a_even, n/2);
-    IFFT(a_odd, n/2);
-
-    // Combine the results using the Cooley-Tukey algorithm
-    for (int k = 0; k < n/2; k++) {
-        complex<double> t = polar(1.0, 2 * M_PI * k / n) * a_odd[k];
-        a[k] = a_even[k] + t;
-        a[k + n/2] = a_even[k] - t;
-    }
-
-    // Clean up
-    delete[] a_even;
-    delete[] a_odd;
-}
+//void IFFT(complex<double> *a, int n) {
+//    // Base case: if n == 1, return
+//    if (n == 1) return;
+//
+//    // Split the array into even and odd halves
+//    complex<double> *a_even = new complex<double>[n/2];
+//    complex<double> *a_odd = new complex<double>[n/2];
+//    for (int i = 0; i < n/2; i++) {
+//        a_even[i] = a[2*i];
+//        a_odd[i] = a[2*i + 1];
+//    }
+//
+//    // Recursively compute the IFFT of the even and odd halves
+//    IFFT(a_even, n/2);
+//    IFFT(a_odd, n/2);
+//
+//    // Combine the results using the Cooley-Tukey algorithm
+//    for (int k = 0; k < n/2; k++) {
+//        complex<double> t = polar(1.0, 2 * M_PI * k / n) * a_odd[k];
+//        a[k] = a_even[k] + t;
+//        a[k + n/2] = a_even[k] - t;
+//    }
+//
+//    // Clean up
+//    delete[] a_even;
+//    delete[] a_odd;
+//}
 // Apply a low-pass filter to the given image
 void lowPassFilter(int *image, int width, int height) {
     // Allocate memory for the complex arrays
@@ -145,8 +257,8 @@ void lowPassFilter(int *image, int width, int height) {
     }
 
     // Compute the FFT of the image
-    FFT(fft_real, width * height);
-    FFT(fft_imag, width * height);
+//    FFT(fft_real, width * height);
+//    FFT(fft_imag, width * height);
 
     // Apply the low-pass filter by setting the high-frequency coefficients to zero
     int n = width * height;
@@ -156,8 +268,8 @@ void lowPassFilter(int *image, int width, int height) {
     }
 
     // Compute the IFFT of the filtered FFT
-    IFFT(fft_real, n);
-    IFFT(fft_imag, n);
+//    IFFT(fft_real, n);
+//    IFFT(fft_imag, n);
 
     // Copy the result back into the image array
     for (int i = 0; i < width * height; i++) {
@@ -180,8 +292,8 @@ void highPassFilter(int *image, int width, int height) {
     }
 
     // Compute the FFT of the image
-    FFT(fft_real, width * height);
-    FFT(fft_imag, width * height);
+   // fft(fft_real, width * height);
+   // fft(fft_imag, width * height);
 
     // Apply the high-pass filter by setting the low-frequency coefficients to zero
     int n = width * height;
@@ -191,8 +303,8 @@ void highPassFilter(int *image, int width, int height) {
     }
 
     // Compute the IFFT of the filtered FFT
-    IFFT(fft_real, n);
-    IFFT(fft_imag, n);
+//    IFFT(fft_real, n);
+//    IFFT(fft_imag, n);
 
     // Copy the result back into the image array
     for (int i = 0; i < width * height; i++) {
